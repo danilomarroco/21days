6bbceeccd55f0d8ed4757eb4061f522791d6ad47
dlmarroco <dlmarroco@gmail.com>
2024-01-21 23:13:37 -0300

    refactor e remocao de codigo desnecessário

--- dia-08/todolist_refactor.py
+++ dia-08/todolist_refactor.py
@@ -5,13 +5,18 @@ def cadastra_tarefa(novo_item):
     return print('TAREFA CADASTRADA COM SUCESSO!!\n')
 
 def listar_tarefas():
-    if len(tarefas) == 0:
-        print('NÃO EXISTEM TAREFAS NA LISTA\n')
+    if len(tarefas) > 0:
+        print('Lista de Tarefas:')
+        for indice, tarefa in enumerate(tarefas):
+            print(f'{indice}. {tarefa}')
     else:
         print(list(enumerate(tarefas, start=1)))
+        print('NÃO EXISTEM TAREFAS NA LISTA\n')
 
-def remove_tarefa(nro_tarefa):
-    if nro_tarefa < len(tarefas) and nro_tarefa >= 0:
+def remove_tarefa(tarefas):
+    listar_tarefas()
+    nro_tarefa = int(input("\nDigite o número da tarefa a ser removida: "))
+    if 0 <= nro_tarefa < len(tarefas):
         tarefas.pop(nro_tarefa)
         return print('TAREFA EXCLUIDA COM SUCESSO!!\n')
     else:
@@ -27,16 +32,16 @@ while True:
     
     match opcao:
         case '1':
-            print('CADASTRO DE TAREFA!!\n')
+            print('\nCADASTRO DE TAREFA!!\n')
             cadastra_tarefa(input('Informe o nome da tarefa: \n'))
         case '2':
-            print('EXCLUSAO DE TAREFA!!\n')
-            remove_tarefa(int(input('Informe o numero da tarefa a ser excluída: ')))
+            print('\nEXCLUSAO DE TAREFA!!\n')
+            remove_tarefa(tarefas)
         case '3':
-            print('LISTAR TAREFAS!!\n')
+            print('\nLISTAR TAREFAS!!\n')
             listar_tarefas()
         case '4':
-            print('Obrigado e volte sempre!!')
+            print('\nObrigado e volte sempre!!')
             break
         case _:
             print('\nOpção inválida\n')
